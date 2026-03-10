58d1723fb323d222408cc3d7cd575f723e9d2935
AJ Cox <aj.cox@students.snow.edu>
2026-03-10 16:11:49 -0600

    Added input validation to lab08

--- Lab08.cpp
+++ Lab08.cpp
@@ -8,14 +8,26 @@ AJ Cox
 #include <iomanip>
 using namespace std;
 
+//This main will ask the user for numbers until they enter 0 and then output the count, min, max and average.
 int main()
 {
 	int count = 0;
 	int num, min = 100, max = -100, sum = 0;
 	double average;
 
+	// Asks for first number.
+	cout << "Hello! Welcome to my program!\n";
 	cout << "Enter an integer (0 to quit): ";
 	cin >> num;
+
+	//Input validation
+	while (num < 0 || num > 10) 
+	{
+		cout << "That is invalid. Please enter a number between 1 and 10 (0 to quit): ";
+		cin >> num;
+	}
+		
+	
 
 	while (num != 0)
 	{
@@ -31,12 +43,22 @@ int main()
 		cout << "Enter another integer (0 to quit): ";
 		cin >> num;
 		
+		//Input validation
+		while (num < 0 || num > 10)
+		{
+			cout << "That is invalid. Please enter a number between 1 and 10 (0 to quit): ";
+			cin >> num;
+		}
+			
+		
 	}
+	//Calculates average
 	average = static_cast<double> (sum) / count;
 
+	//Outputs the count, min, max, and average
 	cout << left;
-	cout << "Count Min   Max   Average" << endl;
+	cout << "\nCount Min   Max   Average" << endl;
 	cout <<  setw(6) << count << setw(6) << min << setw(6) << max << setw(6) << setprecision(2) << fixed << average;
 
-
+	return 0;
 }
